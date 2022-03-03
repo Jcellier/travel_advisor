8d543f37b2fc5bebb4ea814e0934e2ff51fb1fd1
Jordan <jcellierauffret@lilo.org>
2022-03-03 15:10:16 +0100

    Started to render the Map and List, adding functionality

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typograhy, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Map = () => {
-	return <h1>Map</h1>;
+	const classes = useStyles();
+	const isMobile = useMediaQuery("(min-width:600px)");
+
+	const coordinates = { lat: 0, lng: 0 };
+
+	return (
+		<div className={classes.mapContainer}>
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: "AIzaSyBqHlXoxc7BG7RfLb3CKIgzxM108PuqPRM" }}
+				defaultCenter={coordinates}
+				center={coordinates}
+				defaultZoom={14}
+				margin={[50, 50, 50, 50]}
+				options={""}
+				onChange={""}
+				onChildClick={""}
+			></GoogleMapReact>
+		</div>
+	);
 };
 
 export default Map;
